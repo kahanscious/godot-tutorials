9647c30f534ab2cbc0e493e508e31104ab5980df
kahanscious <kahanscious@gmail.com>
2024-12-01 13:36:27 -0500

    cpu particles

--- levels/level.gd
+++ levels/level.gd
@@ -2,11 +2,16 @@ class_name Level extends Node2D
 
 @onready var button: Button = $Button
 @onready var player: Player = $Player
+@onready var rain_manager: RainManager = $RainManager
+@onready var rain_button: Button = $RainButton
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color(0.42, 0, 0.63))
 
+	if rain_manager:
+		rain_manager.intensity = 0.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +24,12 @@ func _on_button_pressed() -> void:
 		var camera: PlayerCamera = player.get_node("Camera2D")
 		camera.configure_shake(Utils.ShakeType.EXPLOSION)
 		camera.add_trauma(0.6)
+
+
+func _on_rain_button_pressed() -> void:
+	if rain_manager.intensity == 0.0:
+		rain_manager.intensity = 1.0
+		rain_button.text = "Stop Rain"
+	else:
+		rain_manager.intensity = 0.0
+		rain_button.text = "Start Rain"
---
+++ rain_manager.gd
@@ -0,0 +1,48 @@
+class_name RainManager extends Node2D
+
+@onready var rain_particles: CPUParticles2D = $CPUParticles2D
+
+@export_range(0.0, 1.0) var intensity: float = 0.5:
+	set(value):
+		intensity = value
+		_update_rain_intensity()
+
+const BASE_PARTICLE_AMOUNT: int = 1000
+
+
+func _ready() -> void:
+	if rain_particles:
+		setup_rain_particles()
+		rain_particles.emitting = false
+		position = Vector2(0, -200)
+
+
+func _process(delta: float) -> void:
+	if rain_particles:
+		var camera: Camera2D = get_node_or_null("/root/TestLevel/Player/Camera2D")
+		if camera:
+			global_position = camera.global_position + Vector2(0, -200)
+
+
+func setup_rain_particles() -> void:
+	rain_particles.amount = BASE_PARTICLE_AMOUNT
+	rain_particles.lifetime = 0.7
+	rain_particles.preprocess = 1.0
+
+	rain_particles.emission_shape = CPUParticles2D.EMISSION_SHAPE_RECTANGLE
+	rain_particles.emission_rect_extents = Vector2(500, 100)
+
+	rain_particles.direction = Vector2(0, 1)
+	rain_particles.spread = 5.0
+	rain_particles.gravity = Vector2(0, 980)
+
+	rain_particles.initial_velocity_min = 400.0
+	rain_particles.initial_velocity_max = 500.0
+
+	rain_particles.scale_amount_min = 0.3
+	rain_particles.scale_amount_max = 0.5
+
+
+func _update_rain_intensity() -> void:
+	if rain_particles:
+		rain_particles.emitting = intensity > 0
