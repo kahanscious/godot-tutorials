11325d9637e8af435df1b2bcdd45ac1b1343d32c
kahanscious <kahanscious@gmail.com>
2024-12-10 11:23:08 -0500

    coyote and buffer

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,7 +9,13 @@ class_name Player extends CharacterBody2D
 @export var air_acceleration: float = 0.1
 @export var jump_force: float = -400.0
 @export var gravity: Vector2 = Vector2(0, 980.0)
+@export var coyote_time: float = 0.2
+@export var jump_buffer_time: float = 0.1
 
+var was_on_floor: bool = false
+var can_coyote_jump: bool = false
+var jump_pressed: bool = false
+var jump_buffer_timer: Timer
 var direction: float
 
 @export var max_health: int = 100
@@ -18,11 +24,22 @@ var current_health: int = 100
 
 func _ready() -> void:
 	current_health = max_health
+	jump_buffer_timer = Timer.new()
+	jump_buffer_timer.one_shot = true
+	jump_buffer_timer.timeout.connect(func(): jump_pressed = false)
+	add_child(jump_buffer_timer)
 
 
 func _physics_process(delta: float) -> void:
+	was_on_floor = is_on_floor()
+
 	if not is_on_floor():
 		velocity += gravity * delta
+
+		if was_on_floor and velocity.y >= 0:
+			can_coyote_jump = true
+			get_tree().create_timer(coyote_time).timeout.connect(func(): can_coyote_jump = false)
+
 		if velocity.y < 0:
 			animations.play("jump")
 			player_particle_manager.stop_run_particles()
@@ -31,10 +48,21 @@ func _physics_process(delta: float) -> void:
 			await animations.animation_finished
 		elif velocity.y > 0:
 			animations.play("fall")
+	else:
+		if jump_pressed:
+			velocity.y = jump_force
+			jump_pressed = false
+			jump_buffer_timer.stop()
+			player_particle_manager.stop_run_particles()
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		player_particle_manager.stop_run_particles()
-		velocity.y = jump_force
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or can_coyote_jump:
+			velocity.y = jump_force
+			can_coyote_jump = false
+			player_particle_manager.stop_run_particles()
+		else:
+			jump_pressed = true
+			jump_buffer_timer.start(jump_buffer_time)
 
 	direction = Input.get_axis("left", "right")
 	if direction:
